f1727e7dcabe7dc8b7fffc823004ae5e18edf229
Alfan Mubarok <alfan@smolhub.com>
2023-10-09 00:52:33 +0700

    Refactor and improve error handling

--- main.go
+++ main.go
@@ -11,62 +11,52 @@ const (
 	exitFailure
 )
 
-// dmenuErorr show error message in dmenu.
-func dmenuErorr(err string) {
-	var cmd = exec.Command("dmenu", "-p", "error: "+err)
-	cmd.Run()
+// exit show error message in dmenu.
+func exit(msg string) {
+	_ = exec.Command("dmenu", "-p", msg).Run()
 	os.Exit(exitFailure)
-}
-
-// trimSpaces trim spaces given value.
-func trimSpaces(value []byte) string {
-	return strings.TrimSpace(string(value))
 }
 
 func main() {
 	var (
-		exitEnter          = "press enter to exit"
-		invalidInputError  = "password cannot empty. " + exitEnter
-		inccorectPassError = "vault: password incorrect. " + exitEnter
-		emptyVaultError    = "vault: no password available"
+		data, secret, selected, passwords string
 	)
 
-	// Ask vault credentials in dmenu.
-	var cmd = exec.Command("dmenu", "-p", "Enter vault password: ", "-P")
-	out, err := cmd.Output()
+	// Ask vault credentials.
+	out, err := exec.Command("dmenu", "-p", "Enter vault passphrase: ", "-P").Output()
 	if err != nil {
-		dmenuErorr("input: " + err.Error())
+		os.Exit(exitFailure)
 	}
 
-	var secret = trimSpaces(out)
-	if secret == "" {
-		dmenuErorr(invalidInputError)
+	if secret = strings.TrimSpace(string(out)); secret == "" {
+		exit("Error: Passphrase cannot empty")
 	}
 
-	// Get available passwords.
+	// Retrieve password names.
 	out, err = exec.Command("skunk", "list", "--pass", secret).Output()
 	if err != nil {
-		dmenuErorr(inccorectPassError)
+		exit("Error: Passphrase is incorrect")
 	}
-	var passwords = strings.ReplaceAll(trimSpaces(out), "- ", "")
+	data = string(out)
 
-	if passwords == emptyVaultError {
-		dmenuErorr(emptyVaultError + ". " + exitEnter)
+	if !strings.Contains(data, "- ") {
+		exit("Vault: No password available")
 	}
+	passwords = strings.ReplaceAll(data, "- ", "")
 
-	// Ask to select password in vault.
-	cmd = exec.Command("dmenu", "-p", "Select a password: ")
+	// Select a password.
+	var cmd = exec.Command("dmenu", "-p", "Select a password: ")
 	cmd.Stdin = strings.NewReader(passwords)
 	out, err = cmd.Output()
 	if err != nil {
-		dmenuErorr("select: " + err.Error())
+		os.Exit(exitFailure)
 	}
-	var selectedPass = trimSpaces(out)
+	selected = strings.TrimSpace(string(out))
 
 	// Copy into clipboard.
-	cmd = exec.Command("skunk", "show", "--name", selectedPass, "--pass", secret, "--copy")
-	if err = cmd.Run(); err != nil {
-		dmenuErorr("copy: " + err.Error())
+	err = exec.Command("skunk", "show", "-name", selected, "-pass", secret, "-copy").Run()
+	if err != nil {
+		exit("Error: Unable to copy password")
 	}
 	os.Exit(exitSuccess)
 }
